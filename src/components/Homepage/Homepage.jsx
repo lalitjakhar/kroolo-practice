074cb0e3ac19d6387d0ebcda60a8f6b5ad87f561
lalitjakhar <lalit25jakhar@gmail.com>
2023-12-14 12:01:05 +0530

    initial commit

---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MiniDrawer from '../Mainheader/Mainheader'
+
+const Homepage = () => {
+  return (
+    <>
+        <MiniDrawer />
+    </>
+  )
+}
+
+export default Homepage
